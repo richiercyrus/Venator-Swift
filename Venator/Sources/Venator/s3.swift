aebbb051b2f4b4862b36a0e7899871c535b93d32
Richie Cyrus <rcyrus@specterops.io>
2020-06-06 10:33:55 -0400

    Removing code that is unneeded

--- Venator/Sources/Venator/s3.swift
+++ Venator/Sources/Venator/s3.swift
@@ -33,16 +33,6 @@ func uploadFile(hash: String) -> String {
     return result
 }
 
-/*
- def amzn_sig(secret_access_key, data, aws_region, aws_service='s3'):
- today = datetime.datetime.utcnow().strftime('%Y%m%d')
- date_key = hmac_sha256('AWS4' + secret_access_key, today)
- date_region_key = hmac_sha256(date_key, aws_region)
- date_region_svc_key = hmac_sha256(date_region_key, aws_service)
- sign_key = hmac_sha256(date_region_svc_key, 'aws4_request')
-
- return hmac.new(sign_key, data, hashlib.sha256).hexdigest()
- */
 func amzn_sig(secret_access_key: String, data: String, aws_region: String) -> String{
     let today: DateFormatter = DateFormatter()
     today.dateFormat = "yyyyMMdd"
@@ -112,11 +102,10 @@ func postS3(venator_data: String) throws -> [String: Any] {
     else {
         file_name = "\(ProcessInfo.processInfo.hostName).json"
     }
-    //let s3_host = "\(String(describing: config.aws_bucket)).s3.\(String(describing: config.aws_region)).amazonaws.com"
     let filename_path = "/uploads/\(file_name)"
     let s3_host = "\(config.aws_bucket!).s3.\(config.aws_region!).amazonaws.com"
     let s3_upload_url = URL.init(string: "https://\(s3_host)\(filename_path)")
-    var results = [String: Any]()
+    let results = [String: Any]()
     var getURLComponents = URLComponents()
     getURLComponents.scheme = "https"
     getURLComponents.host = "s3_host"
@@ -128,9 +117,6 @@ func postS3(venator_data: String) throws -> [String: Any] {
     }
     
     getURLComponents.port = 443
-    let getURL = getURLComponents.url!
-    //print(getURL)
-    //var request = URLRequest(url: getURL)
     var request = URLRequest(url: s3_upload_url!)
     request.httpMethod = "PUT"
     request.httpBody = venator_data.data(using: String.Encoding.utf8)
@@ -148,9 +134,7 @@ func postS3(venator_data: String) throws -> [String: Any] {
     let isoDateFormatter = ISO8601DateFormatter()
     isoDateFormatter.formatOptions = [.withInternetDateTime,
                                       .withFullDate,
-                                      //.withFractionalSeconds,
                                       ]
-    //isoDateFormatter.timeZone = TimeZone.current
     let timestamp = isoDateFormatter.string(from: Date())
     amzn_ts = timestamp.replacingOccurrences(of: "-", with: "").replacingOccurrences(of: ":", with: "").replacingOccurrences(of: ".", with: "")
     
@@ -182,7 +166,7 @@ func postS3(venator_data: String) throws -> [String: Any] {
         if error != nil {
             print("error")
         } else if
-            let data = data,
+            let _ = data,
             let response = response as? HTTPURLResponse, response.statusCode == 200 {
     
             }
@@ -228,103 +212,4 @@ func SHA256(string: String) -> Data {
         }
     }
     return digestData
-}
-
-
-// https://gist.github.com/MihaelIsaev/f913d84b918d2b2c067d
-extension String {
-    var md5: String {
-        return HMAC.hash(inp: self, algo: HMACAlgo.MD5)
-    }
-    
-    var sha1: String {
-        return HMAC.hash(inp: self, algo: HMACAlgo.SHA1)
-    }
-    
-    var sha224: String {
-        return HMAC.hash(inp: self, algo: HMACAlgo.SHA224)
-    }
-    
-    var sha256: String {
-        return HMAC.hash(inp: self, algo: HMACAlgo.SHA256)
-    }
-    
-    var sha384: String {
-        return HMAC.hash(inp: self, algo: HMACAlgo.SHA384)
-    }
-    
-    var sha512: String {
-        return HMAC.hash(inp: self, algo: HMACAlgo.SHA512)
-    }
-}
-
-public struct HMAC {
-    
-    static func hash(inp: String, algo: HMACAlgo) -> String {
-        if let stringData = inp.data(using: String.Encoding.utf8, allowLossyConversion: false) {
-            return hexStringFromData(input: digest(input: stringData as NSData, algo: algo))
-        }
-        return ""
-    }
-    
-    private static func digest(input : NSData, algo: HMACAlgo) -> NSData {
-        let digestLength = algo.digestLength()
-        var hash = [UInt8](repeating: 0, count: digestLength)
-        switch algo {
-        case .MD5:
-            CC_MD5(input.bytes, UInt32(input.length), &hash)
-            break
-        case .SHA1:
-            CC_SHA1(input.bytes, UInt32(input.length), &hash)
-            break
-        case .SHA224:
-            CC_SHA224(input.bytes, UInt32(input.length), &hash)
-            break
-        case .SHA256:
-            CC_SHA256(input.bytes, UInt32(input.length), &hash)
-            break
-        case .SHA384:
-            CC_SHA384(input.bytes, UInt32(input.length), &hash)
-            break
-        case .SHA512:
-            CC_SHA512(input.bytes, UInt32(input.length), &hash)
-            break
-        }
-        return NSData(bytes: hash, length: digestLength)
-    }
-    
-    private static func hexStringFromData(input: NSData) -> String {
-        var bytes = [UInt8](repeating: 0, count: input.length)
-        input.getBytes(&bytes, length: input.length)
-        
-        var hexString = ""
-        for byte in bytes {
-            hexString += String(format:"%02x", UInt8(byte))
-        }
-        
-        return hexString
-    }
-}
-
-enum HMACAlgo {
-    case MD5, SHA1, SHA224, SHA256, SHA384, SHA512
-    
-    func digestLength() -> Int {
-        var result: CInt = 0
-        switch self {
-        case .MD5:
-            result = CC_MD5_DIGEST_LENGTH
-        case .SHA1:
-            result = CC_SHA1_DIGEST_LENGTH
-        case .SHA224:
-            result = CC_SHA224_DIGEST_LENGTH
-        case .SHA256:
-            result = CC_SHA256_DIGEST_LENGTH
-        case .SHA384:
-            result = CC_SHA384_DIGEST_LENGTH
-        case .SHA512:
-            result = CC_SHA512_DIGEST_LENGTH
-        }
-        return Int(result)
-    }
 }
